7f7f278f98138aa7196f83e163d1260a1edf4ec9
Max Ribbans <ribbans.max@gmail.com>
2021-03-19 12:29:26 -0400

    Finish spec

--- server.js
+++ server.js
@@ -28,3 +28,86 @@ app.get('/api/v1/papers', (request, response) => {
       response.status(500).json({ error });
     });
 });
+
+app.get('/api/v1/footnotes', (request, response) => {
+  database('footnotes').select()
+    .then((footnotes) => {
+      response.status(200).json(footnotes);
+    })
+    .catch((error) => {
+      response.status(500).json({ error });
+    });
+});
+
+app.post('/api/v1/papers', (request, response) => {
+  const paper = request.body;
+
+  for (let requiredParameter of ['title', 'author']) {
+    if (!paper[requiredParameter]) {
+      return response
+        .status(422)
+        .send({ error: `Expected format: { title: <String>, author: <String> }. You're missing a "${requiredParameter}" property.` });
+    }
+  }
+
+  database('papers').insert(paper, 'id')
+    .then(paper => {
+      response.status(201).json({ id: paper[0] })
+    })
+    .catch(error => {
+      response.status(500).json({ error });
+    });
+});
+
+app.post('/api/v1/footnotes', (request, response) => {
+  const footnote = request.body;
+
+  for (let requiredParameter of ['paper_id', 'note']) {
+    if (!footnote[requiredParameter]) {
+      return response
+        .status(422)
+        .send({ error: `Expected format: { paper_id: <Integer>, note: <String> }. You're missing a "${requiredParameter}" property.` });
+    }
+  }
+
+  database('footnotes').insert(footnote, 'id')
+    .then(footnote => {
+      response.status(201).json({ id: footnote[0] })
+    })
+    .catch(error => {
+      response.status(500).json({ error });
+    });
+});
+
+// GET a specific paper
+app.get('/api/v1/papers/:id', (request, response) => {
+  database('papers').where('id', request.params.id).select()
+    .then(papers => {
+      if (papers.length) {
+        response.status(200).json(papers);
+      } else {
+        response.status(404).json({
+          error: `Could not find paper with id ${request.params.id}`
+        });
+      }
+    })
+    .catch(error => {
+      response.status(500).json({ error });
+    });
+});
+
+app.get('/api/v1/footnotes/paper/:id', (request, response) => {
+  database('footnotes').where('paper_id', request.params.id).select()
+    .then(footnotes => {
+      if(footnotes.length) {
+        response.status(200).json(footnotes);
+      } else {
+        response.status(404).json({
+          error: `Could not find footnotes with paper_id ${request.params.id}`
+        });
+      }
+    })
+    .catch(error => {
+      response.status(500).json({ error });
+    });
+});
